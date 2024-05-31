bc32d922185039c0e07c15aee9c0ad1276278feb
Vatsal Hariramani <b21234@students.iitmandi.ac.in>
2024-05-31 15:22:13 +0530

    minor bugs

--- train_denoiser_adapter.py
+++ train_denoiser_adapter.py
@@ -207,7 +207,7 @@ def main():
         return
 
 
-    print("Training " +  (count_parameters_trainable(model)/(count_parameters_trainable(model) + count_parameters_total(model)))*100  +"% of the parameters")
+    print("Training " +  str((count_parameters_trainable(model)/(count_parameters_trainable(model) + count_parameters_total(model)))*100)  +"%/ of the parameters")
     print("starting training")
     for epoch in range(starting_epoch, args.epochs):
         before = time.time()
--- utils.py
+++ utils.py
@@ -17,7 +17,7 @@ def count_parameters_trainable(model):
         f.write(summery)
         f.write("\n Total Trainable Params:")
         f.write(str(total_params))
-    print(table)
+    # print(table)
     print(f"Total Trainable Params: {total_params}")
     return total_params
 
@@ -34,7 +34,7 @@ def count_parameters_total(model):
         f.write(summery)
         f.write("\n Total Trainable Params:")
         f.write(str(total_params))
-    print(table)
+    # print(table)
     print(f"Total Params: {total_params}")
     return total_params
 
